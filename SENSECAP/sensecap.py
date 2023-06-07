d0f2029e4baf3913b57dcf24fd7240274a022661
Joao Paulo Coelho <60382778+jpcoelhoATipbDOTpt@users.noreply.github.com>
2023-06-07 14:08:09 +0100

    Update sensecap.py

--- SENSECAP/sensecap.py
+++ SENSECAP/sensecap.py
@@ -1,11 +1,11 @@
 #! /usr/bin/python3
 
 """
-Created on Fri Sep 30 12:43:58 2022
+Modificado em 03/Junho 2023
 
 @author: jpcoelho
 """
-
+import sys
 import minimalmodbus
 import paho.mqtt.client as mqttclient
 import time
@@ -13,7 +13,7 @@ import json
 
 def on_connect(client, usedata,flags,rc):
     if rc==0:
-        print("Client ligado")
+        print("Cliente ligado")
         global connected
         connected=True
     else:
@@ -21,8 +21,9 @@ def on_connect(client, usedata,flags,rc):
         
 # Configuração MQTT
 connected = False
+broker_address = sys.argv[1]
 # broker_address = "192.168.1.68"
-broker_address = "193.136.195.25"
+# broker_address = "193.136.195.25"
 port = 1883
 # user="man4health"
 # password="#Man4Health"
@@ -44,40 +45,39 @@ BAR_REGISTER  = 2
 LUX_REGISTER  = 4
 
 SLAVE_ADDRESS = 14
+try:
+    # Configura instrumento
+    instrument = minimalmodbus.Instrument(PORT,SLAVE_ADDRESS,mode=minimalmodbus.MODE_RTU)
 
-# Configura instrumento
-instrument = minimalmodbus.Instrument(PORT,SLAVE_ADDRESS,mode=minimalmodbus.MODE_RTU)
+    # Parametros do instrumento
+    instrument.serial.baudrate = 9600        # Baud
+    instrument.serial.bytesize = 8
+    instrument.serial.parity   = minimalmodbus.serial.PARITY_NONE
+    instrument.serial.stopbits = 1
+    instrument.serial.timeout  = None          # seconds
 
-# Parametros do instrumento
-instrument.serial.baudrate = 9600        # Baud
-instrument.serial.bytesize = 8
-instrument.serial.parity   = minimalmodbus.serial.PARITY_NONE
-instrument.serial.stopbits = 1
-instrument.serial.timeout  = None          # seconds
+    # Fecha porto
+    instrument.close_port_after_each_call = True
+    instrument.clear_buffers_before_each_transaction = True
 
-# Fecha porto
-instrument.close_port_after_each_call = True
-instrument.clear_buffers_before_each_transaction = True
-
-# Le temperatura
-airTemperature = instrument.read_register(TEMP_REGISTER,2,3,True)
-# Le Humidade
-airMoisture = instrument.read_register(HUM_REGISTER2,3,True)
-# Le Pressão Atmosférica
-MSW = instrument.read_long(BAR_REGISTER,3,True,3)
-atmosphericPressure= (MSW*0.01)
-# Le intensiade de radiação solar
-MSW = instrument.read_long(LUX_REGISTER,3,True,3)
-solarIlluminance= (MSW*0.01)
-solarIrradiance = round(solarIlluminance * 0.008197,2)
-
-# Mostra valores (debug apenas) 
-print('The Air Temperature is: %.1f ºC\r' % airTemperature)
-print('The Air Humidity is: %.1f percent\r' % airMoisture)
-print('The Atmospheric Pressure is: %.1f Pa\r' % atmosphericPressure)
-print('The Light Intensity is: %.1f lux\r' % solarIlluminance)
-print('The Solar Irradiance is: %.1f W/m2\r' % solarIrradiance)
-
+    # Le temperatura
+    airTemperature = instrument.read_register(TEMP_REGISTER,2,3,True)
+    # Le Humidade
+    airMoisture = instrument.read_register(HUM_REGISTER2,3,True)
+    # Le Pressão Atmosférica
+    MSW = instrument.read_long(BAR_REGISTER,3,True,3)
+    atmosphericPressure= (MSW*0.01)
+    # Le intensiade de radiação solar
+    MSW = instrument.read_long(LUX_REGISTER,3,True,3)
+    solarIlluminance= (MSW*0.01)
+    solarIrradiance = round(solarIlluminance * 0.008197,2)
+except:
+    airTemperature = -1
+    airMoisture = -1
+    atmosphericPressure = -1
+    solarIlluminance = -1
+    solarIrradiance = -1
+    
 # Criar payload json
 
 payload = {"AT": airTemperature,
@@ -88,8 +88,6 @@ payload = {"AT": airTemperature,
 
 
 message = json.dumps(payload)
-
-print(message)
 
 # Publicar dados no broker
 try:
